3d0259a4c95e0613e09299983683030bb08b4984
Sebastian Burgos Díaz <sebastianburgos812@hotmail.com>
2015-03-30 13:03:06 -0500

    easydelivery

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=easydelivery',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysql.ultimatefreehost.com;dbname=u901873443_easy',
+    'username' => 'u901873443_easy',
+    'password' => 'easydelivery',
     'charset' => 'utf8',
 ];

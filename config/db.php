a2bd85610edb115d8d97261028f5b90c45afb3cb
Sebastian Burgos Díaz <sebastianburgos812@hotmail.com>
2015-03-30 16:18:48 -0500

    login

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=easydelivery',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=mysql.hostinger.co;dbname=u372348574_easy',
+    'username' => 'u372348574_easy',
+    'password' => 'easydelivery',
     'charset' => 'utf8',
 ];

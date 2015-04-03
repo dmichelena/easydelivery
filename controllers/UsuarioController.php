7028b0a48a9f76b43ddda19d71dea3599040634b
Sebastian <sebastianburgos812@gmail.com>
2015-04-03 16:06:52 -0500

    cambios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -9,6 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
+use app\models\LoginFormUsuario;
 
 /**
  * UsuarioController implements the CRUD actions for Usuario model.

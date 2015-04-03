844d72fffde468ef2dcef4e5fbc369f7e3a80ef4
Sebastian <sebastianburgos812@gmail.com>
2015-04-03 12:40:28 -0500

    cambios

--- views/site/index.php
+++ views/site/index.php
@@ -10,6 +10,10 @@ $this->title = 'My Yii Application';
         <p class="lead">Pide. Trackea. Paga.</p>
     </div>
     
+<?php 
+if(!isset(Yii::$app->user->identity->id))
+{
+?>
     <div class="row">
     	<div class="col-md-6" style="text-align:center">
     		Pedir Delivery
@@ -20,5 +24,11 @@ $this->title = 'My Yii Application';
     		</a>
     	</div>
     </div>
-
+<?php 
+}else{
+?>
+	<div class="row">
+		<h1>Bienvenido <?= Yii::$app->user->identity->username ?></h1>
+	</div>
+<?php }?>
 </div>

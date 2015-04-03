1d5d577e9cdf4823d8f95e276ba2830721b5629c
Sebastian <sebastianburgos812@gmail.com>
2015-04-03 11:12:14 -0500

    cambios

--- views/empresa/superlogin.php
+++ views/empresa/superlogin.php
@@ -1,2 +1,8 @@
-<?php
-echo "ok";
+<div class="row">
+   	<div class="col-md-6" style="text-align:center">
+   		<h1>¿Tienes cuenta?</h1>
+   	</div>
+   	<div class="col-md-6" style="text-align:center">
+   		<h1>Usuario nuevo</h1>
+   	</div>
+</div>

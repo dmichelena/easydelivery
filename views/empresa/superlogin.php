c528726e0f7288dde0b4de42a5550edb48ded3ef
Sebastian <sebastianburgos812@gmail.com>
2015-04-03 11:50:53 -0500

    cambios

--- views/empresa/superlogin.php
+++ views/empresa/superlogin.php
@@ -7,23 +7,12 @@ use yii\bootstrap\ActiveForm;
    	<div class="col-md-6" style="text-align:center">
    		<h1>¿Tienes cuenta?</h1>
    		<div class="site-login" style="text-align:left">		
-		    <?php $form = ActiveForm::begin([
-		        'id' => 'login-form',
-		        'options' => ['class' => 'form-horizontal'],
-		        'fieldConfig' => [
-		            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-		            'labelOptions' => ['class' => 'col-lg-2 control-label'],
-		        ],
-		    ]); ?>
+		    <?php $form = ActiveForm::begin(); ?>
 		
 		    <?= $form->field($modelLogin, 'username') ?>
 		
 		    <?= $form->field($modelLogin, 'password')->passwordInput() ?>
-		
-		    <?= $form->field($modelLogin, 'rememberMe', [
-		        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-		    ])->checkbox() ?>
-		
+				
 		    <div class="form-group">
 		        <div class="col-lg-offset-1 col-lg-11">
 		            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>

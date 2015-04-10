250c5560b8b3af46076413fccbf6bde549d7f72d
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-04-10 11:32:23 -0500

    cambios

--- views/local/_form.php
+++ views/local/_form.php
@@ -20,25 +20,25 @@ use app\models\Turno;
     		],
     ]); ?>
 
-    <?= $form->field($model, 'nombre', ['labelOptions'=>['class'=>'control-label col-md-2'])->textInput(['maxlength' => 100]) ?>
+    <?= $form->field($model, 'nombre', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 100]) ?>
 
-    <?= $form->field($model, 'direccion', ['labelOptions'=>['class'=>'control-label col-md-2'])->textInput(['maxlength' => 100]) ?>
+    <?= $form->field($model, 'direccion', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 100]) ?>
     
     <div id="map_canvas" style="width:400px; height:400px"></div>
 
-    <?= $form->field($model, 'telefono', ['labelOptions'=>['class'=>'control-label col-md-2'])->textInput(['maxlength' => 15]) ?>
+    <?= $form->field($model, 'telefono', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 15]) ?>
 
-    <?= $form->field($model, 'zona_reparto_km', ['labelOptions'=>['class'=>'control-label col-md-2'])->textInput() ?>
+    <?= $form->field($model, 'zona_reparto_km', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput() ?>
 
-    <?= $form->field($model, 'id_turno', ['labelOptions'=>['class'=>'control-label col-md-2'])->dropDownList(ArrayHelper::map(Turno::find()->all(), 'id_turno', 'nombre'), ['prompt' => 'Seleccione una turno']) ?>
+    <?= $form->field($model, 'id_turno', ['labelOptions'=>['class'=>'control-label col-md-2']])->dropDownList(ArrayHelper::map(Turno::find()->all(), 'id_turno', 'nombre'), ['prompt' => 'Seleccione una turno']) ?>
 
-    <?= $form->field($model, 'status', ['labelOptions'=>['class'=>'control-label col-md-2'])->dropDownList([ 'activo' => 'Activo', 'inactivo' => 'Inactivo', ], ['prompt' => '']) ?>
+    <?= $form->field($model, 'status', ['labelOptions'=>['class'=>'control-label col-md-2']])->dropDownList([ 'activo' => 'Activo', 'inactivo' => 'Inactivo', ], ['prompt' => '']) ?>
 
-    <?= $form->field($model, 'id_user', ['labelOptions'=>['class'=>'control-label col-md-2'])->hiddenInput(['value'=> Yii::$app->user->identity->id])->label('') ?>
+    <?= $form->field($model, 'id_user', ['labelOptions'=>['class'=>'control-label col-md-2']])->hiddenInput(['value'=> Yii::$app->user->identity->id])->label('') ?>
     
-    <?= $form->field($model, 'latitud', ['labelOptions'=>['class'=>'control-label col-md-2'])->hiddenInput(['class' => 'latitud'])->label('') ?>
+    <?= $form->field($model, 'latitud', ['labelOptions'=>['class'=>'control-label col-md-2']])->hiddenInput(['class' => 'latitud'])->label('') ?>
     
-    <?= $form->field($model, 'longitud', ['labelOptions'=>['class'=>'control-label col-md-2'])->hiddenInput(['class' => 'longitud'])->label('') ?>
+    <?= $form->field($model, 'longitud', ['labelOptions'=>['class'=>'control-label col-md-2']])->hiddenInput(['class' => 'longitud'])->label('') ?>
     
     <?php 
     	$empresa = Empresa::find()->where('id_user=:id_user',[':id_user' => Yii::$app->user->identity->id])->one();

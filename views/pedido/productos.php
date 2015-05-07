9f6cd8e5f1b11bb57c7a452fb5ccccc7cee2fbb5
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:56:48 -0500

    cambios

--- views/pedido/productos.php
+++ views/pedido/productos.php
@@ -20,7 +20,7 @@
 									<div class="col-md-5">
 										<h3><strong> <?= $m['nombre'] ?> </strong></h3>
 										<em></br>
-											<?= $m['desctipcion'] ?>
+											<?= $m['descripcion'] ?>
 										</em>
 									</div>
 									<div class="col-md-4">

2df23562a942438316f296a6f989bf49e6951c4f
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-07 00:20:59 -0500

    cambios

--- config/web.php
+++ config/web.php
@@ -15,8 +15,8 @@ $config = [
     'components' => [
         'session' => [
             'class' => 'yii\web\DbSession',
-            'db' => 'easydelivery',  // the application component ID of the DB connection. Defaults to 'db'.
-            'sessionTable' => 'my_session', // session table name. Defaults to 'session'.
+            //'db' => 'easydelivery',  // the application component ID of the DB connection. Defaults to 'db'.
+            //'sessionTable' => 'my_session', // session table name. Defaults to 'session'.
         ],
     	'urlManager' => [
     		'enablePrettyUrl' => true,

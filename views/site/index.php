35ca5a82140e1687404a1e5aa1477286861d41be
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:00:41 -0500

    cambios

--- views/site/index.php
+++ views/site/index.php
@@ -1,36 +1,123 @@
-<?php
-/* @var $this yii\web\View */
-$this->title = 'My Yii Application';
-?>
-<div class="site-index">
-
-    <div class="jumbotron">
-        <h1>EasyDelivery</h1>
-
-        <p class="lead">Pide. Trackea. Paga.</p>
-    </div>
-    
-<?php 
-if(!isset(Yii::$app->user->identity->id))
-{
-?>
-    <div class="row">
-    	<div class="col-md-6" style="text-align:center">
-    		<a href="/usuario/superlogin">
-    			Pedir Delivery
-    		</a>
-    	</div>
-    	<div class="col-md-6" style="text-align:center">
-    		<a href="/empresa/superlogin">
-    			Empresas
-    		</a>
-    	</div>
-    </div>
-<?php 
-}else{
-?>
-	<div class="row">
-		<h1>Bienvenido <?= Yii::$app->user->identity->username ?></h1>
+<div class="col-md-12 column">
+			<nav class="navbar navbar-default navbar-inverse" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="#">EasyDelivery</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						
+					<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Rubro<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Carnes y Parrillas</a></li>
+								<li><a href="#">Comida Rápida</a></li>
+                                <li><a href="#">Electrodomésticos</a></li>
+       								<li><a href="#">Licorería</a></li>
+								<li><a href="#">Pizza</a></li>
+							</ul>
+						</li>	
+						
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control">
+						</div> <button type="submit" class="btn btn-default">Buscar</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Iniciar Sesión</a></li>
+						<li class="dropdown">
+							<ul class="dropdown-menu">
+								<li><a href="#">Mi Cuenta</a></li>
+                                <li class="divider"></li>
+								<li><a href="#">Salir</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				
+			</nav>
+			<div class="carousel slide" id="carousel-796873" style="width: 675px; margin: 0 auto">
+				<ol class="carousel-indicators">
+					<li data-slide-to="0" data-target="#carousel-796873">
+					</li>
+					<li data-slide-to="1" data-target="#carousel-796873">
+					</li>
+					<li data-slide-to="2" data-target="#carousel-796873" class="active">
+					</li>
+				</ol>
+				<div class="carousel-inner">
+					<div class="item">
+						<center><img alt="" src="img/electro1.jpg" height="318px" width="675px"></center>
+						<div class="carousel-caption">
+							<h4>ELECTRODOMÉSTICOS</h4>
+						</div>
+					</div>
+					<div class="item">
+						<center><img alt="" src="img/licoreria1.jpg" height="318px" width="675px"></center>
+						<div class="carousel-caption">
+							<h4>LICORERÍAS</h4>
+							</div>
+					</div>
+					<div class="item active">
+						<center><img alt="" src="img/kfcmega1.png" height="318px" width="675px"></center>
+						<div class="carousel-caption">
+							<h4>
+								FAST FOOD
+							</h4>
+						</div>
+					</div>
+				</div> <a class="left carousel-control" href="#carousel-796873" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-796873" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+			</div>
+			<div class="jumbotron">
+				<h1>
+					Bienvenido,
+				</h1>
+				<p>
+				EasyDelivery es la opción más sencilla para realizar tus pedidos  y hacer el seguimiento en tiempo real en el momento que desees.</p>
+				<p>Compra, sigue y paga.</p>
+				<p>
+					¡Pruébalo ya!
+				</p>
+				<p>
+					<a class="btn btn-primary btn-large" href="#">Regístrate ahora</a>
+				</p>
+			</div>
+		</div>
 	</div>
-<?php }?>
-</div>
+	
+    <div class="row clearfix">
+	  <center>
+        <div class="col-md-4 column col-sm-4 col-xs-12">
+			 <address>
+			 <img alt="140x140" src="http://lorempixel.com/140/140/" class="img-circle" >
+          </address>
+			 <address>
+			 <strong>Easy Delivery.</strong><br>
+Avenida La Fontana 1250 <br>
+La Molina, Lima, Perú<br>
+<abbr title="Phone">Telf:</abbr> (+511) 456-7890
+             </address> </br>
+		</div>
+        
+	  <div class="col-md-4 column col-sm-4 col-xs-12">
+			<h4><strong>Lo más visto</strong></h4>
+		  <p>&nbsp;</p>
+			<p><strong>Pollos y Parrillas</strong></p>
+			<p>Roky's</p>
+			<p>Norky's</p>
+		  <p>Villa Chicken</p>
+			<p>Pardo's Chicken</p>
+		  <p>&nbsp; </p>
+		  <p>&nbsp;</p>
+	  </div>
+		<div class="col-md-4 column col-sm-4 col-xs-12">
+		  <h4>&nbsp;</h4>
+          <p>&nbsp;</p>
+          <p><strong>Preguntas frecuentes</strong></p>
+		  <p><strong>Condiciones de uso</strong>          </p>
+		  <p><br>
+		  </p>
+      </div>
+    </center>
+  </div>

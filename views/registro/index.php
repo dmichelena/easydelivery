aba46deccf15355f2f5bffa95609b28624782d31
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:11:00 -0500

    cambios

--- views/registro/index.php
+++ views/registro/index.php
@@ -1,22 +1,40 @@
 <section class="row">
-	<div class="col-md-11">
-		<div class="row">
-			<center>
-				<div class="col-lg-4 col-xs-5 col-md-3">
-					<h4>"Es importante saber donde te ubicas para llevarte tus compras"</h4>
-				</div>
-			</center>
-		<hr/>
-	<div class="row">
-		<div class="col-lg-4 col-xs-12">
-			* Añade Tu dirección: <input type="text" name="firstname" size="60%">
-		</div>
-                   
-		<div class="col-lg-4 col-xs-12">
-			* Ubicar Posición:
-		</div>
+            <div class="col-md-11">
+                <div class="row">
+                	<center>
+                    <div class="col-lg-4 col-xs-5 col-md-3">
+                         <h4>"Es importante saber donde te ubicas para llevarte tus compras"</h4>
+                    </div>
+                    </center>
+                    <div class="col-lg-4 col-xs-2 col-md-4">
+                        <h2></h2>
+                    </div>
+                    <div class="col-lg-4 col-xs-5 col-md-5" ></br>
+                        <button class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-shopping-cart"></span>Seguimiento de Pedido</button><br />
+                        
+                    </div>
+                </div>
+                <hr/>
+                <div class="row">
+                    <div class="col-lg-4 col-xs-12">
+                        * Añade Tu dirección: <input type="text" name="firstname" size="60%">
+                    </div>
+                   <br />
+				   <br />
+				   <br />
+				   <div class="col-lg-4 col-xs-12">
+                        * Ubicar Posición:
+                    </div>
+                        
                     
-		<button type="button"  class="btn btn-primary">Guardar Ubicación</button></div>
-		<hr/>
-	</div>            
-</section>
+                        <img alt="Ubicacion" src="img/geolocalizacion.png" />
+						<br />
+						<br />
+                    <button type="button"  class="btn btn-primary">Guardar Ubicación</button>
+                   
+                    
+                </div>
+                <hr/>
+            </div>	
+            
+        </section>

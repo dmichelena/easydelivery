6b04a6d1f63ec36d80d33ec393f320ae311dc342
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:20:19 -0500

    cambios

--- modules/admin/views/local/_form.php
+++ modules/admin/views/local/_form.php
@@ -17,12 +17,8 @@ use yii\helpers\ArrayHelper;
     <?= $form->field($model, 'nombre')->textInput(['maxlength' => 100]) ?>
 
     <?= $form->field($model, 'direccion')->textInput(['maxlength' => 100]) ?>
-
-    <?= $form->field($model, 'latitud')->textInput(['maxlength' => 13]) ?>
     
     <div class="control-label col-md-2">Seleccione su local en el mapa</div><div class="col-md-10"><div id="map_canvas" style="width:100%; height:400px"></div></div>
-
-    <?= $form->field($model, 'longitud')->textInput(['maxlength' => 13, 'class' => 'latitud']) ?>
 
     <?= $form->field($model, 'telefono')->textInput(['maxlength' => 15, 'class' => 'longitud']) ?>
 
@@ -39,6 +35,10 @@ use yii\helpers\ArrayHelper;
     <?= $form->field($model, 'status')->dropDownList([ 'activo' => 'Activo', 'inactivo' => 'Inactivo', ]) ?>
 
     <?= $form->field($model, 'id_empresa')->hiddenInput(['value' => $session->id])->label(false) ?>
+    
+    <?= $form->field($model, 'latitud', ['labelOptions'=>['class'=>'control-label col-md-2']])->hiddenInput(['class' => 'latitud'])->label('') ?>
+    
+    <?= $form->field($model, 'longitud', ['labelOptions'=>['class'=>'control-label col-md-2']])->hiddenInput(['class' => 'longitud'])->label('') ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

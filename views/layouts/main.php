60d8e3e9f7f01bd5959e05693b0227f52fa368fb
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-07 00:01:57 -0500

    cambios

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,7 +17,7 @@ AppAsset::register($this);
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title>EasyDeliverya</title>
+    <title>EasyDeliver</title>
     <?php $this->head() ?>
 </head>
 <body>

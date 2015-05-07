6f4e8eb1b6feb0b8d9da68017c567b8761ba94d8
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 13:36:50 -0500

    cambios

--- modules/admin/views/producto-local/index.php
+++ modules/admin/views/producto-local/index.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create Producto Local', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Asignar productos', '/admin/local/productos', ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([

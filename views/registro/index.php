01823cedd7c7b9712a8aef7f09dfdfa973313fb5
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:06:12 -0500

    cambios

--- views/registro/index.php
+++ views/registro/index.php
@@ -1,33 +1,22 @@
 <section class="row">
-            <div class="col-md-1">
-                <div class="list-group">
+	<div class="col-md-11">
+		<div class="row">
+			<center>
+				<div class="col-lg-4 col-xs-5 col-md-3">
+					<h4>"Es importante saber donde te ubicas para llevarte tus compras"</h4>
+				</div>
+			</center>
+		<hr/>
+	<div class="row">
+		<div class="col-lg-4 col-xs-12">
+			* Añade Tu dirección: <input type="text" name="firstname" size="60%">
+		</div>
                    
-                </div>
-            </div>
-            <div class="col-md-11">
-                <div class="row">
-                	<center>
-                    <div class="col-lg-4 col-xs-5 col-md-3">
-                         <h4>"Es importante saber donde te ubicas para llevarte tus compras"</h4>
-                    </div>
-                    </center>
-                <hr/>
-                <div class="row">
-                    <div class="col-lg-4 col-xs-12">
-                        * Añade Tu dirección: <input type="text" name="firstname" size="60%">
-                    </div>
-                   <br />
-				   <br />
-				   <br />
-				   <div class="col-lg-4 col-xs-12">
-                        * Ubicar Posición:
-                    </div>
+		<div class="col-lg-4 col-xs-12">
+			* Ubicar Posición:
+		</div>
                     
-                    <button type="button"  class="btn btn-primary">Guardar Ubicación</button>
-                   
-                    
-                </div>
-                <hr/>
-            </div>	
-            
-        </section>
+		<button type="button"  class="btn btn-primary">Guardar Ubicación</button></div>
+		<hr/>
+	</div>            
+</section>

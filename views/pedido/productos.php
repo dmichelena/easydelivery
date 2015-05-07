adb582f94cbd9490e1dc91094ec7f4c8482e036f
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:59:36 -0500

    cambios

--- views/pedido/productos.php
+++ views/pedido/productos.php
@@ -26,7 +26,7 @@
 									<div class="col-md-4">
 										</br>
 										<h3>
-											<a href="/pedido/proceso?id=<?= $m['id_local']?>" type="button" class="btn btn-success"> Ver productos </button>
+											<a href="/pedido/proceso?id=<?= $m['id_local']?>" type="button" class="btn btn-success"> Ver productos </a>
 										</h3>
 									</div>
 								</td>

723de72a2bb108c9c44ce5050e1c9f2b82d05524
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:40:18 -0500

    cambios

--- views/pedido/productos.php
+++ views/pedido/productos.php
@@ -1,55 +1,77 @@
-<section class="row">
-            <div class="col-lg-3">
-                <div class="list-group">
-                    <a href="#" class="list-group-item ">Megas</a>
-                    <a href="#" class="list-group-item ">Combos</a>
-                    <a href="#" class="list-group-item ">Tostados y Sandwiches</a>
-                    <a href="#" class="list-group-item ">Postres</a>
-                </div>
-            </div>
-            <div class="col-lg-9">
-                <div class="row">
-                	<center>
-                    <div class="col-lg-4 col-xs-3 col-md-3">
-                        <img src="http://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/1024px-KFC_logo.svg.png" height="80" width="80" /> <h2>KFC</h2>
-                    </div>
-                    </center>
-                    <div class="col-lg-4 col-xs-4 col-md-4">
-                        <h2></h2>
-                    </div>
-                    <div class="col-lg-4 col-xs-5 col-md-5" ></br>
-                        <button class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-shopping-cart"></span>Carrito de Compra</button><br />
-                        <label>Sub-Total: </label>
-                        <label>S/. 0.00 </label>
-                    </div>
-                </div>
-                <hr/>
-                <div class="row">
-                    <div class="col-lg-4">
-                        <img alt="140x140" src="img/productos/productos-twistertradicional.png" class="img-rounded" />
-                    </div>
-                    <div class="col-lg-4">
-                        
-                            <label style="color: #FFBD32"><u>Combo Twister Tradicional</u></label>
-                            <label class="control-label">1 Twister tradicional </label>
-                            <br />
-                            <label class="control-label">1 Papa mediana </label>
-                            <br />
-                            <label class="control-label">1 Gaseosa mediana </label>
-                            <br/>
-                            <label class="control-label">Precio S/16.90 </label>
-                    </div>
-                    <div class="col-lg-4">
-                        <form class=" form-inline">
-                            <div class="input-group input-group-sm">
-                            <span class="input-group-addon" id="sizing-addon1">Cantidad</span>
-                            <input type="number" min="1" class="form-control" aria-describedby="sizing-addon1" value="1" />
-                        </div></br>
-                        <button type="button" class="btn btn-lg btn-warning" >Comprar</button>
-                        </form>
-                    </div>
-                </div>
-                <hr/>
-            </div>	
-            
-        </section>
+<div class="row">
+                	<div >
+                	  <h5>
+                          <div>
+                                <div class="row clearfix">
+                                  <div class="col-md-12 column col-xs-12">
+                                        <table style="text-align:center;" class="table table-condensed table-hover">
+                                            <thead>
+                                                <tr class="active" >
+                                                    <th><h3><strong>  Locales de Pizza</strong></h3></th>                                                  </tr>
+                                            </thead>
+                                            <tbody class="active" >
+                                              <tr >
+                                                    <td >
+                                                        <div class="col-md-3">
+                                                     <h2><img  src="img/lapastara1.png" width="100" height="100" class="img-circle"> </h2>
+                                                    </div>
+                                                    <div class="col-md-5">
+                                                     <h3><strong> La Pastana </strong></h3>
+                                                     <em></br>
+                                                     Pizza, Comida saludable
+                                                     </em></div>
+                                                    <div class="col-md-4">
+                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
+													</h3>
+                                                    </div>
+                                                    </td>
+                                                    
+                                                </tr>
+                                                <tr >
+                                                    <td >
+                                                        <div class="col-md-3">
+                                                     <h2><img  src="img/greenpizza1.jpg" width="100" height="100" class="img-circle"> </h2>
+                                                    </div>
+                                                    <div class="col-md-5">
+                                                     <h3><strong> Greens Pizza </strong></h3>
+                                                     <em></br>
+                                                     Pizza, Comida saludable
+                                                     </em></div>
+                                                    <div class="col-md-4">
+                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
+													</h3>
+                                                    </div>
+                                                    </td>
+                                                    
+                                                </tr>
+                                                <tr >
+                                                    <td >
+                                                        <div class="col-md-3">
+                                                     <h2><img  src="img/papajohns1.jpg" width="100" height="100" class="img-circle"> </h2>
+                                                    </div>
+                                                    <div class="col-md-5">
+                                                     <h3><strong> Papa John's </strong></h3>
+                                                     <em></br>
+                                                     Pizza, Comida rápida
+                                                     </em></div>
+                                                    <div class="col-md-4">
+                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
+													</h3>
+                                                    </div>
+                                                    </td>
+                                                    
+                                                </tr>
+                                                                                                
+                                                
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                        </div>   
+                   	  </h5>
+                    
+                                                    
+                    
+               	</div>
+		
+			</div>

6de2dc67dbb2c0cbdbf0fcfd91f2e20cb2729ab7
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 13:37:20 -0500

    cambios

--- modules/admin/views/producto-local/index.php
+++ modules/admin/views/producto-local/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id_producto',
+            'Producto.nombre',
             'id_local',
             'stock',
             'precio',

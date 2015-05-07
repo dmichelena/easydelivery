c4e58e90114951d3aba030c61c1eb9f1950e5878
Sebastian Burgos Díaz <sebastianburgos812@gmail.com>
2015-05-07 14:49:35 -0500

    cambios

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -2,6 +2,8 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\Local;
+use yii\db\Query;
 class PedidoController extends Controller
 {
 	public function actionProductos()
@@ -12,6 +14,30 @@ class PedidoController extends Controller
 		{
 			return $this->redirect("/registro");
 		}
+		
+		$sql = "
+			SELECT 
+				nombre, 
+				( 3959 * acos( cos( radians('%s') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( lat ) ) ) ) AS distance 
+			FROM 
+				local
+			HAVING 
+				distance < '100' 
+			ORDER BY 
+				distance 
+			LIMIT 0 , 20";
+		$sql = sprintf($sql, 
+				$session['usuario-web']['latitud'],
+				$session['usuario-web']['longitud'],
+				$session['usuario-web']['latitud']
+		);
+		echo "<pre>";print_r($sql);die();
+  
+		
+		$model = (new Query())
+					->select('address, name, lat, lng, ( 3959 * acos( cos( radians('%s') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( lat ) ) ) ) AS distance')
+					->from('local')
+					->where('')
 		
 		return $this->render("productos");
 	}
--- views/pedido/productos.php
+++ views/pedido/productos.php
@@ -1,77 +1,40 @@
 <div class="row">
-                	<div >
-                	  <h5>
-                          <div>
-                                <div class="row clearfix">
-                                  <div class="col-md-12 column col-xs-12">
-                                        <table style="text-align:center;" class="table table-condensed table-hover">
-                                            <thead>
-                                                <tr class="active" >
-                                                    <th><h3><strong>  Locales de Pizza</strong></h3></th>                                                  </tr>
-                                            </thead>
-                                            <tbody class="active" >
-                                              <tr >
-                                                    <td >
-                                                        <div class="col-md-3">
-                                                     <h2><img  src="img/lapastara1.png" width="100" height="100" class="img-circle"> </h2>
-                                                    </div>
-                                                    <div class="col-md-5">
-                                                     <h3><strong> La Pastana </strong></h3>
-                                                     <em></br>
-                                                     Pizza, Comida saludable
-                                                     </em></div>
-                                                    <div class="col-md-4">
-                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
-													</h3>
-                                                    </div>
-                                                    </td>
-                                                    
-                                                </tr>
-                                                <tr >
-                                                    <td >
-                                                        <div class="col-md-3">
-                                                     <h2><img  src="img/greenpizza1.jpg" width="100" height="100" class="img-circle"> </h2>
-                                                    </div>
-                                                    <div class="col-md-5">
-                                                     <h3><strong> Greens Pizza </strong></h3>
-                                                     <em></br>
-                                                     Pizza, Comida saludable
-                                                     </em></div>
-                                                    <div class="col-md-4">
-                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
-													</h3>
-                                                    </div>
-                                                    </td>
-                                                    
-                                                </tr>
-                                                <tr >
-                                                    <td >
-                                                        <div class="col-md-3">
-                                                     <h2><img  src="img/papajohns1.jpg" width="100" height="100" class="img-circle"> </h2>
-                                                    </div>
-                                                    <div class="col-md-5">
-                                                     <h3><strong> Papa John's </strong></h3>
-                                                     <em></br>
-                                                     Pizza, Comida rápida
-                                                     </em></div>
-                                                    <div class="col-md-4">
-                                                     </br><h3><button type="button" class="btn btn-success"> Ver productos </button>
-													</h3>
-                                                    </div>
-                                                    </td>
-                                                    
-                                                </tr>
-                                                                                                
-                                                
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                        </div>   
-                   	  </h5>
-                    
-                                                    
-                    
-               	</div>
-		
-			</div>
+	<div>
+		<h5>
+			<div>
+				<div class="row clearfix">
+					<div class="col-md-12 column col-xs-12">
+						<table style="text-align:center;" class="table table-condensed table-hover">
+						<thead>
+							<tr class="active" >
+								<th><h3><strong>Locales cerca a ti</strong></h3></th>
+							</tr>
+						</thead>
+						<tbody class="active" >
+							<tr>
+								<td>
+									<div class="col-md-3">
+										<h2><img  src="img/lapastara1.png" width="100" height="100" class="img-circle"> </h2>
+									</div>
+									<div class="col-md-5">
+										<h3><strong> La Pastana </strong></h3>
+										<em></br>
+											Pizza, Comida saludable
+										</em>
+									</div>
+									<div class="col-md-4">
+										</br>
+										<h3>
+											<button type="button" class="btn btn-success"> Ver productos </button>
+										</h3>
+									</div>
+								</td>
+							</tr>                                                                                            
+						</tbody>
+						</table>
+					</div>
+				</div>
+			</div>   
+		</h5>
+	</div>
+</div>

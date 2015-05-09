bea5357659a0ea5be0df75fba91c556a3fbf4357
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-09 00:36:56 -0500

    cambios

--- component/MenuHelper.php
+++ component/MenuHelper.php
@@ -5,11 +5,13 @@ class MenuHelper
 {
     public static function getMenu()
     {
-        return [
+        $response[] = [
                     'label' => 'ola',
                     'url' => ['#'],
                     'items' => '<li class="dropdown-header">Dropdown Header</li>',
                     '<li class="divider"></li>',
                 ];
+
+        return $response;
     }
 }

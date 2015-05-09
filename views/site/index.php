ba5fa1f4512cd6301bb326077a9f13f714e2a660
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-09 00:21:52 -0500

    cambios

--- views/site/index.php
+++ views/site/index.php
@@ -1,85 +1,40 @@
 <div class="col-md-12 column">
-			<div class="carousel slide" id="carousel-796873" style="width: 675px; margin: 0 auto">
-				<ol class="carousel-indicators">
-					<li data-slide-to="0" data-target="#carousel-796873">
-					</li>
-					<li data-slide-to="1" data-target="#carousel-796873">
-					</li>
-					<li data-slide-to="2" data-target="#carousel-796873" class="active">
-					</li>
-				</ol>
-				<div class="carousel-inner">
-					<div class="item">
-						<center><img alt="" src="img/electro1.jpg" height="318px" width="675px"></center>
-						<div class="carousel-caption">
-							<h4>ELECTRODOMÉSTICOS</h4>
-						</div>
-					</div>
-					<div class="item">
-						<center><img alt="" src="img/licoreria1.jpg" height="318px" width="675px"></center>
-						<div class="carousel-caption">
-							<h4>LICORERÍAS</h4>
-							</div>
-					</div>
-					<div class="item active">
-						<center><img alt="" src="img/kfcmega1.png" height="318px" width="675px"></center>
-						<div class="carousel-caption">
-							<h4>
-								FAST FOOD
-							</h4>
-						</div>
-					</div>
-				</div> <a class="left carousel-control" href="#carousel-796873" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-796873" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	<div class="carousel slide" id="carousel-796873" style="width: 675px; margin: 0 auto">
+		<ol class="carousel-indicators">
+			<li data-slide-to="0" data-target="#carousel-796873"></li>
+            <li data-slide-to="1" data-target="#carousel-796873"></li>
+            <li data-slide-to="2" data-target="#carousel-796873" class="active"></li>
+        </ol>
+		<div class="carousel-inner">
+			<div class="item">
+				<img alt="" src="img/electro1.jpg" height="318px" width="675px">
+                <div class="carousel-caption">
+					<h4>ELECTRODOMÉSTICOS</h4>
+				</div>
 			</div>
-			<div class="jumbotron">
-				<h1>
-					Bienvenido,
-				</h1>
-				<p>
-				EasyDelivery es la opción más sencilla para realizar tus pedidos  y hacer el seguimiento en tiempo real en el momento que desees.</p>
-				<p>Compra, sigue y paga.</p>
-				<p>
-					¡Pruébalo ya!
-				</p>
-				<p>
-					<a class="btn btn-primary btn-large" href="/registro">Regístrate ahora</a>
-				</p>
+			<div class="item">
+				<img alt="" src="img/licoreria1.jpg" height="318px" width="675px">
+                <div class="carousel-caption">
+					<h4>LICORERÍAS</h4>
+				</div>
+			</div>
+			<div class="item active">
+				<img alt="" src="img/kfcmega1.png" height="318px" width="675px">
+                <div class="carousel-caption">
+					<h4>
+						FAST FOOD
+					</h4>
+				</div>
 			</div>
 		</div>
+            <a class="left carousel-control" href="#carousel-796873" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-796873" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+	<div class="jumbotron">
+		<h1>Bienvenido,</h1>
+        <p>EasyDelivery es la opción más sencilla para realizar tus pedidos  y hacer el seguimiento en tiempo real en el momento que desees.</p>
+        <p>Compra, sigue y paga.</p>
+		<p>¡Pruébalo ya!</p>
+        <p><a class="btn btn-primary btn-large" href="/registro">Regístrate ahora</a></p>
 	</div>
-	
-    <div class="row clearfix">
-	  <center>
-        <div class="col-md-4 column col-sm-4 col-xs-12">
-			 <address>
-			 <img alt="140x140" src="http://lorempixel.com/140/140/" class="img-circle" >
-          </address>
-			 <address>
-			 <strong>Easy Delivery.</strong><br>
-Avenida La Fontana 1250 <br>
-La Molina, Lima, Perú<br>
-<abbr title="Phone">Telf:</abbr> (+511) 456-7890
-             </address> </br>
-		</div>
-        
-	  <div class="col-md-4 column col-sm-4 col-xs-12">
-			<h4><strong>Lo más visto</strong></h4>
-		  <p>&nbsp;</p>
-			<p><strong>Pollos y Parrillas</strong></p>
-			<p>Roky's</p>
-			<p>Norky's</p>
-		  <p>Villa Chicken</p>
-			<p>Pardo's Chicken</p>
-		  <p>&nbsp; </p>
-		  <p>&nbsp;</p>
-	  </div>
-		<div class="col-md-4 column col-sm-4 col-xs-12">
-		  <h4>&nbsp;</h4>
-          <p>&nbsp;</p>
-          <p><strong>Preguntas frecuentes</strong></p>
-		  <p><strong>Condiciones de uso</strong>          </p>
-		  <p><br>
-		  </p>
-      </div>
-    </center>
-  </div>
+</div>
+

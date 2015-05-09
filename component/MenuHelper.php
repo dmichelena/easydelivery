39f840514d8877b9eee9a0a239c61ddee6fc3f96
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-09 00:42:18 -0500

    cambios

--- component/MenuHelper.php
+++ component/MenuHelper.php
@@ -1,15 +1,34 @@
 <?php
 namespace app\component;
 
+use app\models\Rubro;
+use yii\db\Query;
+
 class MenuHelper
 {
     public static function getMenu()
     {
+        $session = \Yii::$app->session;
+
+        if(!$session->has('locales-web'))
+        {
+            return null;
+        }
+
+        $data = (new Query())
+            ->select('*')
+            ->from('rubro')
+            ->join("INNER JOIN", "empresa", "empresa.id_rubro = rubro.id_rubro")
+            ->join("INNER JOIN", "local", "local.id_empresa = empresa.id_empresa")
+            ->where()
+
         $response[] = [
                     'label' => 'ola',
                     'url' => ['#'],
-                    'items' => ['<li class="dropdown-header">Dropdown Header</li>',
-                    '<li class="divider"></li>',]
+                    'items' => [
+                        '<li class="dropdown-header">Dropdown Header</li>',
+                        '<li class="divider"></li>',
+                    ]
                 ];
 
         return $response;

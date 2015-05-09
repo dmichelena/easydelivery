34fc28b744ccdb01dae528e9fae291b721b40c58
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-09 04:18:08 -0500

    cambios

--- views/pedido/resumen.php
+++ views/pedido/resumen.php
@@ -108,7 +108,7 @@ use yii\widgets\ActiveForm;
 
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10" >
-                    <button type="submit" class="btn btn-primary">Confirmar Compra</button>
+                    <?=CHtml::submitButton('Save', array('confirm'=>'¿Confirma la compra?'));?>
                 </div>
             </div>
 

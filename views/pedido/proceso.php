9ce6ee3d2ffaa369b73689929b7ad2373aac5e11
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-09 01:05:05 -0500

    cambios

--- views/pedido/proceso.php
+++ views/pedido/proceso.php
@@ -7,7 +7,7 @@ use yii\widgets\ActiveForm;
 		<?php 
 			foreach($categoria as $c):
 		?>
-			<a href="<?=$c['id'] ?>" class="list-group-item "><?=$c['nombre'] ?></a>
+			<a href="<?=$c['id_categoria'] ?>" class="list-group-item "><?=$c['nombre'] ?></a>
 		<?php endforeach;?>
 		</div>
 	</div>

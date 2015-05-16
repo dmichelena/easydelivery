f3d33f26d7a44acec914c990d324bcb58cbc67fa
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-16 01:43:34 -0500

    cambios

--- views/site/index.php
+++ views/site/index.php
@@ -33,23 +33,28 @@
 <div class="col-md-4 column">
     <div class="usuario-form">
 
-        <?php $form = \yii\widgets\ActiveForm::begin(); ?>
+        <?php $form = \yii\widgets\ActiveForm::begin([
+            'options' => ['class' => 'form-horizontal'],
+            'fieldConfig' => [
+                'template' => "{label}\n<div class=\"col-md-10\">{input}</div>\n<div class=\"col-md-offset-2 col-md-10\">{error}</div>",
+            ],
+        ]); ?>
 
-        <?= $form->field($model, 'nombre')->textInput(['maxlength' => 45]) ?>
+        <?= $form->field($model, 'nombre', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 45]) ?>
 
-        <?= $form->field($model, 'apellido_p')->textInput(['maxlength' => 45]) ?>
+        <?= $form->field($model, 'apellido_p', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 45]) ?>
 
-        <?= $form->field($model, 'apellido_m')->textInput(['maxlength' => 45]) ?>
+        <?= $form->field($model, 'apellido_m', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 45]) ?>
 
-        <?= $form->field($model, 'password')->passwordInput(['maxlength' => 45]) ?>
+        <?= $form->field($model, 'password', ['labelOptions'=>['class'=>'control-label col-md-2']])->passwordInput(['maxlength' => 45]) ?>
 
-        <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
+        <?= $form->field($model, 'fecha_nacimiento', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput() ?>
 
-        <?= $form->field($model, 'dni')->textInput(['maxlength' => 8]) ?>
+        <?= $form->field($model, 'dni', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 8]) ?>
 
-        <?= $form->field($model, 'correo')->textInput(['maxlength' => 45]) ?>
+        <?= $form->field($model, 'correo', ['labelOptions'=>['class'=>'control-label col-md-2']])->textInput(['maxlength' => 45]) ?>
 
-        <?= $form->field($model, 'status')->dropDownList([ 'activo' => 'Activo', 'inactivo' => 'Inactivo', ]) ?>
+        <?= $form->field($model, 'status', ['labelOptions'=>['class'=>'control-label col-md-2']])->dropDownList([ 'activo' => 'Activo', 'inactivo' => 'Inactivo', ]) ?>
 
         <div class="form-group">
             <?= \yii\helpers\Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>

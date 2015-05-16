2381a99d4e3a1eb099e391f4b2b07cab5f60d1b3
Sebastian Burgos <sebastianburgos812@gmail.com>
2015-05-16 03:59:27 -0500

    cambios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -7,11 +7,21 @@ class UsuarioController extends \yii\web\Controller
 {
     public function actionPedidos()
     {
+        if(!$session->has('usuario-webos'))
+        {
+            return $this->redirect("/");
+        }
+
         $model = (new Query())
             ->select("*")
             ->from("delivery")
             ->where([
-
+                'id_usuario' => $session['usuario-webos']->id_usuario,
             ])
+            ->all();
+
+        return $this->render("pedidos", [
+            'model' => $model,
+        ])
     }
 }
---
+++ views/usuario/pedidos.php
@@ -0,0 +1,47 @@
+<div class="col-md-12 column">
+    <font size="+3"><span class="glyphicon glyphicon-shopping-cart">Seguimiento de mis Pedidos</span></font>
+</div>
+</div>
+<div class="row clearfix">
+    <div class="col-md-12 column">
+        <div class="table-responsive">
+            <table class="table table-hover table-bordered">
+                <thead>
+                <tr class="active">
+                    <th>N° Producto</th>
+                    <th>Fecha</th>
+                    <th>Tiempo Aproximado de Entrega</th>
+                    <th>Detalle</th>
+                    <th>Estado</th>
+                    <th>Seguimiento</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>112</td>
+                    <td>Abril 10 12:45 PM</td>
+                    <td>Abril 10 1:30PM</td>
+                    <td> <button type="button" class="btn btn-primary">Ver Detalle</button> </td>
+                    <td>En Camino</td>
+                    <td><button type="button" class="btn btn-primary">Ver</button></td>
+                </tr>
+                <tr>
+                    <td>113</td>
+                    <td>Abril 12 2:30PM</td>
+                    <td>Abril 10 3:00PM</td>
+                    <td><button type="button" class="btn btn-primary">Ver Detalle</button></td>
+                    <td>En Camino</td>
+                    <td><button type="button" class="btn btn-primary">Ver</button></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="row clearfix"></div>
+        <div class="row" ></div>
+
+        <div class="row clearfix"></div>
+    </div>
+</div> <br></br><br></br>
+</div>

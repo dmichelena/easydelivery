f53103294cc19be1d0805033269d11bbf4163865
Diego Michelena Polo <damp1991@gmail.com>
2015-05-30 11:32:20 -0500

    Update UsuarioController.php

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -26,8 +26,7 @@ class UsuarioController extends \yii\web\Controller
             'model' => $model,
         ]);
     }
-
-    public function actionDetalle($id_delivery)
+    public function actionSeguimiento()
     {
         $session = \Yii::$app->session;
 
@@ -35,7 +34,6 @@ class UsuarioController extends \yii\web\Controller
         {
             return $this->redirect("/");
         }
-<<<<<<< HEAD
         $get = \Yii::$app->request->get();
         if(empty($get)){
             return $this->redirect("/usuario/pedidos");
@@ -54,26 +52,9 @@ class UsuarioController extends \yii\web\Controller
         return $this->render("seguimiento", [
             'pedido' => $pedido[0],
         ]);
-=======
 
-        $model = (new Query())
-            ->select("*")
-            ->from("delivery")
-            ->join("INNER JOIN", "pedido", "pedido.id_delivery = delivery.id_delivery")
-            ->join("INNER JOIN", "local", "local.id_local = pedido.id_local")
-            ->join("INNER JOIN", "producto", "producto.id_producto = pedido.id_producto")
-            ->where([
-                'delivery.id_delivery' => $id_delivery
-            ])
-            ->all();
->>>>>>> d5bc26dd6d992f579183fd2170647fde9ee9f3c5
-
-        return $this->render("detalle",[
-            'model' => $model
-        ]);
     }
-
-    public function actionSeguimiento()
+    public function actionDetalle()
     {
         $session = \Yii::$app->session;
 
@@ -81,6 +62,7 @@ class UsuarioController extends \yii\web\Controller
         {
             return $this->redirect("/");
         }
+
 
     }
 }

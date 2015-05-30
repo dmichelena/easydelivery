d5bc26dd6d992f579183fd2170647fde9ee9f3c5
sebastian <sebastianburgos812@gmail.com>
2015-05-30 07:32:10 -0500

    cambios

--- modules/admin/views/reporte/index.php
+++ modules/admin/views/reporte/index.php
@@ -160,8 +160,8 @@
                     <div style="text-align: center">
                         <h2>Zonas Comunes</h2><br />
                     </div>
-                    <div class="col-lg-4 col-xs-12">
-                        <div id="map-canvas" style="width:100%;height: 200px;"></div>
+                    <div class="col-lg-12 col-xs-12">
+                        <div id="map-canvas" style="width:100%;height: 300px;"></div>
                     </div>
                     <br />
                     <br />

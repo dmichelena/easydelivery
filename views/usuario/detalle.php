00f58bf5407d7e140093afddbc99eb771afde2f5
sebastian <sebastianburgos812@gmail.com>
2015-05-30 02:31:17 -0500

    detalle pedido

--- views/usuario/detalle.php
+++ views/usuario/detalle.php
@@ -1,3 +1,75 @@
-<div class="col-lg-3 col-md-3 col-xs-6">
-    <button class="btn btn-danger btn-lg"><span class="glyphicon glyphicon-shopping-cart"></span>Seguimiento de Pedidos</button><br />
+<div class="row">
+    <div class="col-lg-6 col-md-6 col-xs-12">
+        <table class="table table-condensed table-hover">
+            <tbody>
+            <tr>
+                <td><strong>Número de Pedido</strong></td>
+                <td>100123</td>
+            </tr>
+            <tr>
+                <td><strong>Fecha Pedido</strong></td>
+                <td>09/04/2015</td>
+            </tr>
+            <tr>
+                <td><strong>Nombre</strong></td>
+                <td>Javier Guere</td>
+            </tr>
+            <tr>
+                <td><strong>Teléfono</strong></td>
+                <td>01 3652161</td>
+            </tr>
+            <tr>
+                <td><strong>Dirección</strong></td>
+                <td>Av. Aviación 3351 - San Borja</td>
+            </tr>
+            <tr>
+                <td><strong>Tiempo de Entrega (aprox)</strong></td>
+                <td>Abril 9,  13:00</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <br />
+    <br />
+
+    <div class="col-lg-6 col-md-6 col-xs-12">
+        <table class="table table-condensed table-hover">
+            <thead>
+            <tr class="active">
+                <th>Detalle</th>
+                <th>Precio unidad</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>Megatrío</td>
+                <td>S/. 39.00</td>
+                <td>1</td>
+                <td>S/. 39.00</td>
+            </tr>
+            <tr>
+                <td>Mega Famiiar</td>
+                <td>S/. 49.00</td>
+                <td>1</td>
+                <td>S/. 49.00</td>
+            </tr>
+            <tr>
+                <td>Costo de envío</td>
+                <td></td>
+                <td></td>
+                <td>S/. 0.00</td>
+            </tr>
+            <tr>
+                <td><strong>Total</strong></td>
+                <td></td>
+                <td></td>
+                <td><strong>S/. 88.00</strong></td>
+            </tr>
+            </tbody>
+        </table>
+        <br />
+        <br />
+    </div>
 </div>

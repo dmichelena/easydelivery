b7c519f110c2eeb5f7925a741e2254fae98414d8
sebastian <sebastianburgos812@gmail.com>
2015-05-30 06:59:33 -0500

    cambios

--- modules/admin/views/reporte/index.php
+++ modules/admin/views/reporte/index.php
@@ -47,7 +47,7 @@
                         <h2>Productos mas vendidos</h2><br />
                     </div>
                     <div class="col-lg-4 col-xs-12"></div>
-                    <img alt="Ubicacion" src="http://girlfromoutofthisworld.com/wp-content/uploads/2013/01/GoogleCharts.png" />
+                    <div id="chart_div"></div>
                     <br />
                     <br />
 
@@ -177,3 +177,39 @@
     </div>
 </div>
 </div>
+
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+<script>
+    // Load the Visualization API and the piechart package.
+    google.load('visualization', '1.0', {'packages':['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.setOnLoadCallback(drawChart);
+
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+            ['Mushrooms', 3],
+            ['Onions', 1],
+            ['Olives', 1],
+            ['Zucchini', 1],
+            ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+            'width':400,
+            'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+    }
+</script>

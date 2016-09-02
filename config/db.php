09c2ab20c355281db6858ec94dc058dce2554e92
Diego Michelena <damp1991@gmail.com>
2016-09-02 00:12:56 -0500

    WIP

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 $user = getenv('USER_DB') ? getenv('USER_DB') : 'root';
 $pass = getenv('PASS_DB') ? getenv('PASS_DB') : '';
 $db = getenv('DB') ? getenv('DB') : 'root';
-$host = getenv('HOST_DB') ? getenv('HOST_DB') : 'root';
+$host = getenv('HOST_DB') ? getenv('HOST_DB') : '127.0.0.1';
 return [
     'class' => 'yii\db\Connection',
     'dsn' => "mysql:host={$host};dbname={$db}",
